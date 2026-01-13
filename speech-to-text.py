59c09def123f7e9f4a40f2c976c5c7e879dc5c41
adrdeatcu <adideatcu340@gmail.com>
2026-01-13 18:57:19 +0200

    install pyaudio and speechrecognition libraries

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,1 +1,7 @@
-print("Hello from speech-to-text module!")
+import pyaudio
+import speech_recognition as sr
+
+print("PyAudio Version:", pyaudio.__version__)
+print("SpeechRecognition Version:", sr.__version__)
+r=sr.Recognizer()
+print("Librarile au fost importate cu succes.")
