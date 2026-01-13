ac3db44300f88e2627feda0ab3c4705a2d856f75
adrdeatcu <adideatcu340@gmail.com>
2026-01-13 23:53:05 +0200

    create gui with customtkinter and speech recognition function for romanian

--- speech-to-text.py
+++ speech-to-text.py
@@ -1,7 +1,44 @@
-import pyaudio
+import customtkinter as ctk 
 import speech_recognition as sr
+import threading
 
-print("PyAudio Version:", pyaudio.__version__)
-print("SpeechRecognition Version:", sr.__version__)
-r=sr.Recognizer()
-print("Librarile au fost importate cu succes.")
+
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("dark-blue")
+
+class App:
+    def __init__(self):
+        self.root = ctk.CTk()
+        self.root.title("Speech to Text")
+        self.root.geometry("600x400")
+
+        self.label = ctk.CTkLabel(self.root, text="Speech to Text", font=ctk.CTkFont(size=20))
+        self.label.pack(pady=20)
+
+        self.btn = ctk.CTkButton(self.root, text="Record", command=self.recognize_speech)
+        self.btn.pack(pady=10)
+
+        self.result = ctk.CTkTextbox(self.root, width=400, height=200)
+        self.result.pack(pady=20)
+
+    def recognize_speech(self):
+        def run():
+            r = sr.Recognizer() #initializam recunoasterea vocala 
+            mic = sr.Microphone() #initializam microfonul
+            with mic as source:
+                r.adjust_for_ambient_noise(source)
+                audio = r.listen(source)
+            try:
+                text = r.recognize_google(audio, language='ro-RO')
+                self.result.insert("end", text + "\n")
+            except:
+                self.result.insert("end", "Repeat the sentece\n")
+        threading.Thread(target=run).start()  # Evită blocarea GUI
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    app = App() #crearea unei instante a clasei App
+    app.run() #pornirea aplicatiei
